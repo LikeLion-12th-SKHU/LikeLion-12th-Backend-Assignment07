64f262bc187ade509c6f97962d59809c9dd5d757
Kang Duoh <kduoh99@gmail.com>
2024-05-26 18:45:55 +0900

    feat: 사용자 정의 JPQL 쿼리 작성

--- kangduoh/likelion-assignment-crud/src/main/java/org/likelion/likelionassignmentcrud/category/domain/repository/CategoryItemRepository.java
+++ kangduoh/likelion-assignment-crud/src/main/java/org/likelion/likelionassignmentcrud/category/domain/repository/CategoryItemRepository.java
@@ -3,9 +3,13 @@ package org.likelion.likelionassignmentcrud.category.domain.repository;
 import org.likelion.likelionassignmentcrud.category.domain.CategoryItem;
 import org.likelion.likelionassignmentcrud.item.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CategoryItemRepository extends JpaRepository<CategoryItem, Long> {
+    @Query("SELECT ci.category.name " +
+            "FROM CategoryItem ci " +
+            "WHERE ci.item = :item")
     List<CategoryItem> findByItem(Item item);
 }
