deb92d30d5f29172088fb6b2e170667d536bb9ca
seoyun <seoyun-cho@naver.com>
2024-05-28 17:14:10 +0900

    과제 제출

---
+++ seoyun/likelion--recrud/src/main/java/org/likelion/likelionrecrud/item/application/ItemService.java
@@ -0,0 +1,62 @@
+package org.likelion.likelionrecrud.item.application;
+
+import lombok.RequiredArgsConstructor;
+import org.likelion.likelionrecrud.item.api.dto.request.ItemSaveReqDto;
+import org.likelion.likelionrecrud.item.api.dto.request.ItemUpdateReqDto;
+import org.likelion.likelionrecrud.item.api.dto.response.ItemInfoResDto;
+import org.likelion.likelionrecrud.item.api.dto.response.ItemListResDto;
+import org.likelion.likelionrecrud.item.domain.Item;
+import org.likelion.likelionrecrud.item.domain.repository.ItemRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void itemSave(ItemSaveReqDto itemsaveReqDto) {
+        Item item = Item.builder()
+                .name(itemsaveReqDto.name())
+                .price(itemsaveReqDto.price())
+                .stockQuantity(itemsaveReqDto.stockQuantity())
+                .build();
+        itemRepository.save(item);
+    }
+
+    public ItemListResDto itemFindAll() {
+        List<Item> items = itemRepository.findAll();
+        List<ItemInfoResDto> itemInfoResDtoList = items.stream()
+                .map(ItemInfoResDto::from)
+                .toList();
+        return ItemListResDto.from(itemInfoResDtoList);
+    }
+
+    public ItemInfoResDto itemFindOne(Long itemId) {
+        Item item = itemRepository.findById(itemId).orElseThrow
+                (() -> new IllegalArgumentException("해당 상품이 없습니다."));
+        return ItemInfoResDto.from(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, ItemUpdateReqDto itemUpdateReqDto){
+        Item item = itemRepository.findById(itemId).orElseThrow(
+                () ->new IllegalArgumentException("해당 상품이 없습니다.")
+                );
+        item.update(itemUpdateReqDto);
+    }
+
+    @Transactional
+    public void ItemDelete(Long itemId){
+        Item item = itemRepository.findById(itemId).orElseThrow(
+                ()-> new IllegalArgumentException("해당 상품이 없습니다.")
+        );
+        itemRepository.delete(item);
+    }
+
+}
